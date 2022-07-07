7db3c142a627294401108a87a8b516360f39767a
Harold McCloud <hpmccloud@gmail.com>
2022-07-07 02:41:15 -0400

    Default project files

---
+++ desktop/src/com/corporatekoi/eaglesprout/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.corporatekoi.eaglesprout;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.corporatekoi.eaglesprout.EagleSprout;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Eagle Sprout");
+		new Lwjgl3Application(new EagleSprout(), config);
+	}
+}
