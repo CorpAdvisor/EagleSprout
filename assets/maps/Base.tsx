e95ed3a23a89edab6af71d4551fc8d437cc1eaa3
Harold McCloud <hpmccloud@gmail.com>
2022-07-10 19:09:02 -0400

    Organizing files

--- assets/maps/Base.tsx
+++ assets/maps/Base.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.0" name="Base" tilewidth="64" tileheight="32" tilecount="672" columns="16">
- <image source="tilesheet0.png" trans="ffffff" width="1024" height="1344"/>
+ <image source="../graphics/tiles/tilesheet0.png" trans="ffffff" width="1024" height="1344"/>
 </tileset>
